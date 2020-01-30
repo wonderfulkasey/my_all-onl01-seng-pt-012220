4f66c207e0a6a586565f24de2089f5611785e72c
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 21:49:45 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,11 @@ def my_all?(collection)
   while i < collection.length 
   block_return_values << yield(collection[i])
   i = i + 1
-  
   end
 
+  if
+    block_return_values.include?(false)
+  else
+    true
+  end
 end
